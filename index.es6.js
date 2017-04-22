d7762d46d2b6079d25fcc66c370741fcb7ec7049
Jibin Mathews <jibinmathews7@gmail.com>
2017-04-22 23:54:39 +0530

    Server initialization

--- index.es6.js
+++ index.es6.js
@@ -1,6 +1,16 @@
-import * as express from 'express';
-import * as http from 'http';
-import * as socket from 'socket.io';
+import express from 'express';
+import http from 'http';
+import socket from 'socket.io';
+import logTable from './compiled/table';
+
 
 global.console.table = logTable;
+
+const app = express();
+
+const server = http.createServer(app);
+
+server.listen(process.env.PORT || 3000, ()=>{
+  console.log("Server listenting on port "+(process.env.PORT || 3000));
+});
 
