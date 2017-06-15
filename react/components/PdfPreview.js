98a2ac8e2583aadc8e6966275551bd769f84bf20
Jibin Mathews <jibinmathews7@gmail.com>
2017-06-15 22:35:55 +0530

    PdfPreview url fix

--- react/components/PdfPreview.js
+++ react/components/PdfPreview.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import Encrypter from '../../compiled/modules/utils/crypto';
+
 import Constants from '../constants';
 
 export default class PdfPreview extends React.Component {
   render () {
     return (
-      <iframe className="pdf-preview" src={Constants.BASE_URL + `/file/raw/${this.props.content.name}?path=` + this.props.content.path} />
+      <iframe className="pdf-preview" src={Constants.BASE_URL + `/file/raw/${this.props.content.name}?path=` + Encrypter.encryptString(this.props.content.path)} />
     )
   }
 };
